9ac01b7d9e334ad1ef5a2bc20a30da9c9e073697
Jeferson Vieira <jeferson.vm1981@gmail.com>
2021-04-04 00:59:23 -0300

    atualização 4

---
+++ revisao-csharp/Conceito.cs
@@ -0,0 +1,15 @@
+namespace revisao_csharp
+{
+    public enum Conceito
+    {
+        A,
+
+        B,
+
+        C,
+
+        D,
+
+        E
+    }
+}
--- revisao-csharp/Program.cs
+++ revisao-csharp/Program.cs
@@ -39,9 +39,53 @@ namespace revisao_csharp
                         break;
                     case "2":
                         //TODO: listar alunos
+                        foreach (var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota} "); 
+                                
+                            }    
+                                                       
+                        }
                         break;
                     case "3":
                         //TODO: calcular média geral
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+                        for (int i=0; i < alunos.Length; i++)
+                        {
+                            if (!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notaTotal = notaTotal + alunos[i].Nota;
+                                nrAlunos++;
+                            }
+                        } 
+                        var mediaGeral = notaTotal / nrAlunos;
+                        Conceito conceitoGeral;
+                        if (mediaGeral < 2)
+                        {
+                            conceitoGeral = Conceito.E;                            
+                        }
+                        else if (mediaGeral < 4)
+                        {
+                            conceitoGeral = Conceito.D;
+                        }
+                         else if (mediaGeral < 6)
+                        {
+                            conceitoGeral = Conceito.C;
+                        }
+                         else if (mediaGeral < 8)
+                        {
+                            conceitoGeral = Conceito.B;
+                        }
+                         else 
+                        {
+                            conceitoGeral = Conceito.A;
+                        }
+
+                        Console.WriteLine($" MÉDIA GERAL: {mediaGeral} - CONCEITO: {conceitoGeral}");
+
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
